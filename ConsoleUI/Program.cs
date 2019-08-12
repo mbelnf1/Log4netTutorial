2511de6991272bd9fcfe8908f49662308c72f6de
mbelnf1 <mbeln1992@gmail.com>
2019-08-11 22:31:36 -0400

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+[assembly: log4net.Config.XmlConfigurator(Watch=true)]
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World");
+
+            //log.Error("This is my error message");
+            log.Debug("developer: Tutorial was run");
+            log.Info("Maintenance: water pump turned on");
+            log.Warn("Maintenance: the water pump is getting hot");
+
+            var i = 0;
+
+            try
+            {
+                var x = 10 / i;
+            }
+            catch (DivideByZeroException ex)
+            {
+                log.Error("Developer: we tried to divide by zero again");
+            }
+            Counter j = new Counter();
+
+            log4net.GlobalContext.Properties["Counter"] = j;
+            for (j.LoopCounter = 0; j.LoopCounter < 5; j.LoopCounter++)
+            {
+                log.Fatal("This is a fatal error in the process");
+            }
+            //log.Fatal("Maintenance: water pump exploded");
+
+            Console.ReadLine();
+        }
+    }
+}
